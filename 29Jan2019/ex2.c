8e1526dc9812ffab7913b518a4372c0fb77f5a72
gtfarng <5735512002@psu.ac.th>
2019-01-29 23:07:02 +0700

    update_lab2-2

--- 29Jan2019/ex2.c
+++ 29Jan2019/ex2.c
@@ -1,21 +1,13 @@
 #include <stdio.h> 
-#include<stdlib.h>
 #include <math.h> 
 int main()  
 {     
- 	int num,pos,tmp;
-        double cal;
-        
-          
+ 	int num,pos;     
 	printf("Please enter a number (6 digits): ");     
 	scanf ("%d",&num);     
 	printf("Please select the position (1-6): ");     
 	scanf ("%d",&pos);           
-	//tmp=pos-1;
-        cal=pow(10,pos);
-
-        printf("The digit is %f\n",cal);         
+	printf("The digit is %d\n",num/(int)pow(10,pos-1)%10);         
 	
 	return 0;    
 }
-	
