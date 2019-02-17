539ad459a255f1407434a1e36bc1b359f7c16bd2
gtfarng <5735512002@psu.ac.th>
2019-02-17 15:19:39 +0700

    update

---
+++ code/star_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	
+	printf("Enter the number of rows: ");
+	scanf("%d",&n);
+		
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(j+1<=n-i)
+			 	printf("*");
+			else
+				printf(" ");
+		}
+			printf("\n");
+	}
+	
+	
+	return 0;
+}
